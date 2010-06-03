f6463c1c91de4d8245396d5f9f4fbac2a18bb0e4
René Bählkow <reba@projekt182.de>
2010-06-03 07:18:56 +0200

    updated gpl header

--- src/ID3v2Reader.cpp
+++ src/ID3v2Reader.cpp
@@ -1,3 +1,26 @@
+/* vi:set enc=utf-8 ts=4 sw=4: */
+
+/***********************************************************************
+ *                                                                     *
+ *  Copyright (C) 2006 -2010  René Bählkow <reba@projekt182.de>        *
+ *                                                                     *
+ *  This file is part of Plow.                                         *
+ *                                                                     *
+ *  Plow is free software; you can redistribute it and/or              *
+ *  modify it under the terms of the GNU General Public License        *
+ *  as published by the Free Software Foundation; either version 2     *
+ *  of the License, or (at your option) any later version.             *
+ *                                                                     *
+ *  Plow is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *  GNU General Public License for more details.                       *
+ *                                                                     *
+ *  You should have received a copy of the GNU General Public License  *
+ *  along with Plow.  If not, see <http://www.gnu.org/licenses/>       *
+ *                                                                     *
+ ***********************************************************************/
+
 #include "ID3v2Reader.h"
 
 #include <tag.h>
--- src/ReaderSelector.h
+++ src/ReaderSelector.h
@@ -1,21 +1,25 @@
-/************************************************************************
- * Copyright (C) 2006 - 2009 by René Bählkow                            *
- *                                                                      *
- * This program is free software; you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2 of the License, or    *
- * (at your option) any later version.                                  *
- *                                                                      *
- * This program is distributed in the hope that it will be useful,      *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
- * GNU General Public License for more details.                         *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with this program; if not, write to the                        *
- * Free Software Foundation, Inc.,                                      *
- * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.            *
- ************************************************************************/
+/* vi:set enc=utf-8 ts=4 sw=4: */
+
+/***********************************************************************
+ *                                                                     *
+ *  Copyright (C) 2006 -2010  René Bählkow <reba@projekt182.de>        *
+ *                                                                     *
+ *  This file is part of Plow.                                         *
+ *                                                                     *
+ *  Plow is free software; you can redistribute it and/or              *
+ *  modify it under the terms of the GNU General Public License        *
+ *  as published by the Free Software Foundation; either version 2     *
+ *  of the License, or (at your option) any later version.             *
+ *                                                                     *
+ *  Plow is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *  GNU General Public License for more details.                       *
+ *                                                                     *
+ *  You should have received a copy of the GNU General Public License  *
+ *  along with Plow.  If not, see <http://www.gnu.org/licenses/>       *
+ *                                                                     *
+ ***********************************************************************/
 
 #ifndef PLOW_READER_SELECTOR_H
 #define PLOW_READER_SELECTOR_H
